54fb56523ee23ed58f0c76f331586308f1197bf6
5.cr4b <62923541+5cr4b@users.noreply.github.com>
2022-08-26 14:56:53 +0700

    Create self_CA.sh

---
+++ self_CA.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+DOMAIN=$1
+
+# Create root CA & Private key
+
+openssl req -x509 \
+            -sha256 -days 356 \
+            -nodes \
+            -newkey rsa:2048 \
+            -subj "/CN=${DOMAIN}/C=VN/L=HaNoi" \
+            -keyout rootCA.key -out rootCA.crt 
+
+# Generate Private key 
+
+openssl genrsa -out ${DOMAIN}.key 2048
+
+# Create csf conf
+
+cat > csr.conf <<EOF
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C = VN
+ST = HN
+L = HN
+O = KMA
+OU = k16KMA
+CN = ${DOMAIN}
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = ${DOMAIN}
+DNS.2 = www.${DOMAIN}
+
+EOF
+
+# create CSR request using private key
+openssl req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr -config csr.conf
+
+# Create a external config file for the certificate
+cat > cert.conf <<EOF
+
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${DOMAIN}
+
+EOF
+# Create SSl with self signed CA
+openssl x509 -req \
+    -in ${DOMAIN}.csr \
+    -CA rootCA.crt -CAkey rootCA.key \
+    -CAcreateserial -out ${DOMAIN}.crt \
+    -days 365 \
+    -sha256 -extfile cert.conf
